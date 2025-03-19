2119d408c6f61efa933d973515061c82e6f6f38f
Devansh-Awasthi <devanshawasthi03@gmail.com>
2025-03-19 12:28:07 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
 function App() {
   return (
-
-        <BrowserRouter> 
- <Routes>
-        <Route path='/' element={<Home/>}></Route>
-</Routes>
-        </BrowserRouter>
-
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/room" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-
-export default App
+export default App;
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [Name, setName] = useState("");
+  const [Room, setRoomId] = useState("");
+  const HandelSubmit =(e) => {
+    e.preventDefault();
+    console.log(Name);
+    console.log(Room);
+  }
   return (
     <div className='bg-[#F3F4F8] h-screen w-full'>
-        Home Page
+      <form onSubmit={HandelSubmit} method="post">
+        <input type='text' value={Name} onChange={(e)=>{
+            setName(e.target.value);
+        }}></input>
+        <input type='text' name={Room}  onChange={(e)=>{
+            setRoomId(e.target.value);}}></input>
+        <button type='submit' className='bg-amber-600'> submit</button>
+        </form>
     </div>
   )
 }
