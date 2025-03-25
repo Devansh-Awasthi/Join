443f9ce1c89491d0f459f234ae111f6e5674b8a4
Devansh-Awasthi <devanshawasthi03@gmail.com>
2025-03-25 20:54:41 +0530

    backend and frontend connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var socket_io_1 = require("socket.io");
-var io = new socket_io_1.Server(3000);
+var io = new socket_io_1.Server(3000, {
+    cors: {
+        origin: "http://localhost:5173", // Change this to your frontend URL
+        methods: ["GET", "POST"],
+        // credentials: true,  
+    },
+});
 io.on("connection", function (socket) {
     console.log('Hello from server');
+    socket.on('room:join', function (name, roomId) {
+        socket.join(roomId);
+        console.log("".concat(name, " joined room ").concat(roomId));
+    });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import { Server } from "socket.io";
-const io = new Server(3000);
+const io = new Server(3000,{
+    cors: {
+        origin: "http://localhost:5173", // Change this to your frontend URL
+        methods: ["GET", "POST"],
+        // credentials: true,  
+    },
+});
 
 io.on("connection", (socket) => {
  console.log('Hello from server');
+ socket.on('room:join',(name,roomId)=>{
+    socket.join(roomId);
+    console.log(`${name} joined room ${roomId}`);
+ })
 })
